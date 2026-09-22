9a69ccd3a72ff7478b1ca2339e0e63921ca20390
botoddly <250804054+botoddly@users.noreply.github.com>
2026-09-22 14:40:02 +0200

    Relicense to zlib and fix attribution of the WebGPU backend (#3)

--- src/SDL_gpu_webgpu.c
+++ src/SDL_gpu_webgpu.c
@@ -1,3 +1,32 @@
+/*
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2026 Sam Lantinga <slouken@libsdl.org>
+
+  Original WebGPU backend
+  Copyright (C) 2025-2026 The Stickmahn <thestickmahn@proton.me>
+
+  Further changes
+  Copyright (C) 2026 Stan (stanoddly)
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+// Adapted from the original WebGPU backend to build outside the SDL tree. See README.md for the list of changes.
+
 // TODO:
 // - Clean up this code base.
 //  - This code base is miserable to read through. I've placed functions in random places without care in the world.
